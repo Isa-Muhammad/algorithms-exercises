93b5f3d3de9185feef4195e452aae2e65995fd6e
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-01-21 12:27:39 +0100

    Implement merge-sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,35 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // base case
+  if (nums.length < 2) return nums;
+
+  // break into two smaller arrays
+  const middle = Math.floor(nums.length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+
+  // return the merge of both arrays and call mergesort on both (recusive call)
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return results.concat(right, left);
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
